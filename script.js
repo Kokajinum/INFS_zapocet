84be258271645d501aaf4aac02938d1b942b66cc
Kokajinum <petulikoz@gmail.com>
2021-06-29 22:14:59 +0200

    dodelani galerie

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+
+var gallery = new SimpleLightbox(".gallery a", { /* options */ });
+
 var cardIndex = 0;
 
 var text1 = [];
 text1[0] = "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.";
-text1[1] = "Et harum quidem rerum facilis est et expedita distinctio. Fusce wisi. Praesent vitae arcu tempor neque lacinia pretium. Morbi imperdiet, mauris.";
-text1[2] = "Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Morbi imperdiet, mauris.";
+text1[1] = "Et harum quidem rerum facilis est et expedita distinctio. Fusce wisi. Praesent vitae arcu tempor neque lacinia pretium. Morbi imperdiet, velit turpis quidem quidem.";
+text1[2] = "Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Morbi imperdiet, mauris velit turpis quidem quidem.";
 
 var text2 = [];
 text2[0] = "Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus.";
@@ -49,3 +52,8 @@ function changeText() {
     p3[0].innerText = names[cardIndex];
     p4[0].innerText = roles[cardIndex];
 }
+
+function scrollUp() {
+    document.documentElement.scrollTop = 0;
+}
+
